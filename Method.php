914b8c7bc2f5d000ebf6d0b6bf060deb84820ecd
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-10 05:34:05 +0300

    no message

---
+++ Method.php
@@ -0,0 +1,13 @@
+<?php
+namespace Dfe\IPay88;
+// 2017-04-10
+final class Method extends \Df\PaypalClone\Method {
+	/**
+	 * 2017-04-10
+	 * @override
+	 * @see \Df\Payment\Method::amountLimits()
+	 * @used-by \Df\Payment\Method::isAvailable()
+	 * @return null
+	 */
+	protected function amountLimits() {return null;}
+}
