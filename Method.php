bf2cffcff1c2e6d2bd22e40b9a10f1a6af7ec714
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-10 06:01:56 +0300

    no message

--- Method.php
+++ Method.php
@@ -13,6 +13,8 @@ final class Method extends \Df\PaypalClone\Method {
 
 	/**
 	 * 2017-04-10
+	 * @used-by https://github.com/mage2pro/ipay88/blob/0.0.2/etc/di.xml#L9
+	 * @used-by https://github.com/mage2pro/ipay88/blob/0.0.2/etc/frontend/di.xml#L16
 	 * @used-by \Df\Payment\Method::codeS()
 	 * @see \Df\Payment\Settings::prefix()
 	 */
