d0bfd71164b13dedce09eeabd89d20340b7e71f5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-10 08:13:37 +0300

    no message

---
+++ Charge.php
@@ -0,0 +1,35 @@
+<?php
+namespace Dfe\IPay88;
+/**
+ * 2017-04-10
+ * @method Method m()
+ * @method Settings s()
+ */
+final class Charge extends \Df\PaypalClone\Charge {
+	/**
+	 * 2017-04-10
+	 * @override
+	 * @see \Df\PaypalClone\Charge::pCharge()
+	 * @used-by \Df\PaypalClone\Charge::p()
+	 * @return array(string => mixed)
+	 */
+	protected function pCharge() {$s = $this->s(); return [];}
+
+	/**
+	 * 2017-04-10
+	 * @override
+	 * @see \Df\PaypalClone\Charge::k_RequestId()
+	 * @used-by \Df\PaypalClone\Charge::p()
+	 * @return string
+	 */
+	protected function k_RequestId() {return '';}
+
+	/**
+	 * 2017-04-10
+	 * @override
+	 * @see \Df\PaypalClone\Charge::k_Signature()
+	 * @used-by \Df\PaypalClone\Charge::p()
+	 * @return string
+	 */
+	protected function k_Signature() {return '';}
+}
