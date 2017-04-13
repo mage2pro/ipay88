8489e9046b359bc44f286e7908a17a6200770b59
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-04-13 07:18:41 +0300

    no message

--- Block/Info.php
+++ Block/Info.php
@@ -1,7 +1,11 @@
 <?php
 namespace Dfe\IPay88\Block;
-// 2017-04-10
-/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+use Dfe\IPay88\W\Event;
+/**
+ * 2017-04-10
+ * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
+ * @method Event|string|null e(...$k)
+ */
 class Info extends \Df\Payment\Block\Info {
 	/**
 	 * 2017-04-10
@@ -9,5 +13,7 @@ class Info extends \Df\Payment\Block\Info {
 	 * @see \Df\Payment\Block\Info::prepare()
 	 * @used-by \Df\Payment\Block\Info::_prepareSpecificInformation()
 	 */
-	final protected function prepare() {}
+	final protected function prepare() {
+		$this->siEx('iPay88 ID', $this->e()->idE());
+	}
 }
