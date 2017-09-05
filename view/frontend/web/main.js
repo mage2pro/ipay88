e0d7f210094a2c816741f6325d37717eed5f0969
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-06 02:32:00 +0300

    1.1.16

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,6 +1,3 @@
 // 2017-04-10
-define([
-	'Df_Payment/withOptions'
-], function(parent) {'use strict'; return parent.extend({
-	defaults: {df: {test: {showBackendTitle: false}}}
-});});
+/** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */
+define(['Df_Payment/withOptions'], function(parent) {'use strict'; return parent.extend();});
