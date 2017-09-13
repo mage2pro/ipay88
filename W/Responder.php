406b8bf254179864d6a1a8f5e77ad2d73b3a81a1
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-13 08:40:30 +0300

    1.2.0

--- W/Responder.php
+++ W/Responder.php
@@ -1,8 +1,8 @@
 <?php
 namespace Dfe\IPay88\W;
-use Df\Framework\Controller\Result\Text;
-// 2017-04-12
-final class Handler extends \Df\PaypalClone\W\Handler {
+use Df\Framework\Controller\Response\Text;
+// 2017-09-13
+final class Responder extends \Df\Payment\W\Responder {
 	/**
 	 * 2017-04-12
 	 * «On the ‘backend_response.asp’ page you need to write out the word ‘RECEIVEOK’ only (without quote)
@@ -12,9 +12,9 @@ final class Handler extends \Df\PaypalClone\W\Handler {
 	 * on different interval if no ‘RECEIVEOK’ acknowledgement detected.»
 	 * «Technical Specification v1.6.2 (For Malaysia Only)» Page 10. https://mage2.pro/t/3682
 	 * @override
-	 * @see \Df\Payment\W\Handler::result()
-	 * @used-by \Df\Payment\W\Handler::handle()
+	 * @see \Df\Payment\W\Responder::success()
+	 * @used-by \Df\Payment\W\Responder::get()
 	 * @return Text
 	 */
-	protected function result() {return Text::i('RECEIVEOK');}
+	protected function success() {return Text::i('RECEIVEOK');}
 }
