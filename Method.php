1eca6476abc0d665ef55d7620923dfcc4c2e4b97
Dmitry Fedyuk <dfediuk@gmail.com>
2023-01-04 16:25:22 +0000

    https://github.com/mage2pro/core/issues/168

--- Method.php
+++ Method.php
@@ -14,9 +14,8 @@ final class Method extends \Df\PaypalClone\Method {
 	/**
 	 * 2017-04-14
 	 * @used-by \Dfe\IPay88\Charge::pCharge()
-	 * @return string
 	 */
-	function option() {return df_result_sne($this->iia(self::$II_OPTION));}
+	function option():string {return df_result_sne($this->iia(self::$II_OPTION));}
 
 	/**
 	 * 2017-04-10
